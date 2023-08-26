4419beb9b6e763e92d6320b651778ba2a6182791
PK2_Stimpy <thefernan1@gmail.com>
2023-08-26 17:01:28 +0200

    Added bounty message when joining

--- src/main/java/me/pk2/canalosaland/listeners/PlayerListener.java
+++ src/main/java/me/pk2/canalosaland/listeners/PlayerListener.java
@@ -2,10 +2,15 @@ package me.pk2.canalosaland.listeners;
 
 import static me.pk2.canalosaland.util.Wrapper.*;
 
+import me.pk2.canalosaland.CanelonesCore;
+import me.pk2.canalosaland.config.buff.ConfigBountyBuffer;
 import me.pk2.canalosaland.config.buff.ConfigMainBuffer;
 import me.pk2.canalosaland.user.User;
 import me.pk2.canalosaland.user.UserManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -19,6 +24,17 @@ public class PlayerListener implements Listener {
 
         if(ConfigMainBuffer.buffer.messages.join.enabled)
             event.setJoinMessage(_COLOR(_PLACEHOLDER(event.getPlayer(), ConfigMainBuffer.buffer.messages.join.message)));
+
+        String playerName = event.getPlayer().getName();
+        Bukkit.getScheduler().runTaskLater(CanelonesCore.INSTANCE, () -> {
+            Player player = Bukkit.getPlayer(playerName);
+            if(player == null)
+                return;
+
+            double bounty = ConfigBountyBuffer.getBounty(player.getUniqueId().toString());
+            if(bounty > 0)
+                Bukkit.getOnlinePlayers().forEach(p -> p.sendMessage(_COLOR("&7" + p.getName() + " &8has a &7" + bounty + "$ &8bounty.")));
+        }, 100L);
     }
 
     @EventHandler
