de48e00dcbf12f5b8c05c4d001d2c1bff58548d2
PK2-Stimpy <pk2stimpy@furryporn.fun>
2023-11-27 19:51:48 +0100

    lol

--- src/main/java/me/pk2/canalosaland/interfaces/GInterfaceShop.java
+++ src/main/java/me/pk2/canalosaland/interfaces/GInterfaceShop.java
@@ -1,8 +1,16 @@
 package me.pk2.canalosaland.interfaces;
 
+import static me.pk2.canalosaland.util.Wrapper.*;
+
 import me.pk2.canalosaland.db.buffer.DBBufferShops;
 import me.pk2.canalosaland.db.buffer.shop.DBBSShop;
+import me.pk2.canalosaland.db.buffer.shop.DBBSShopData;
+import me.pk2.canalosaland.db.buffer.shop.items.DBBSShopItem;
 import me.pk2.canalosaland.user.User;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
 
 public class GInterfaceShop extends GInterface {
     public GInterfaceShop(User user) {
@@ -31,6 +39,18 @@ public class GInterfaceShop extends GInterface {
         if(shop == null) {
             owner.sendLocale("INTERFACE_SHOP_ERROR_INSTANCE");
             return;
+        }
+
+        DBBSShopData shopData = shop.getShopData();
+        for(int i = 0; i < shopData.getItems().size(); i++) {
+            DBBSShopItem item = shopData.getItems().get(i);
+
+            ItemStack stack = item.getMaterial().clone();
+            ItemMeta meta = stack.getItemMeta();
+            meta.setLore(Arrays.asList("", _COLOR(String.format("&e%.2f$", item.getPrice())), owner.translateC("INTERFACE_SHOP_PURCHASE")));
+            stack.setItemMeta(meta);
+
+            setItem(i, stack);
         }
     }
 }
